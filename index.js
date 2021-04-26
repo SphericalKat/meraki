a7ee47828f01739694539d3b77a3754a439ba2e1
SphericalKat <amolele@gmail.com>
2021-04-27 01:49:31 +0530

    chore(assets): use Inter as default app font
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- index.js
+++ index.js
@@ -4,17 +4,48 @@
 import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
-import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+import {
+  configureFonts,
+  DefaultTheme,
+  Provider as PaperProvider,
+} from 'react-native-paper';
 import {name as appName} from './app.json';
 
-const peach = '#FCBCC7';
+const fontFamily = {
+  regular: {
+    fontFamily: 'Inter-Regular',
+    fontWeight: 'normal',
+  },
+
+  medium: {
+    fontFamily: 'Inter-Medium',
+    fontWeight: 'normal',
+  },
+
+  light: {
+    fontFamily: 'Inter-Light',
+    fontWeight: 'normal',
+  },
+
+  thin: {
+    fontFamily: 'Inter-Thin',
+    fontWeight: 'normal',
+  },
+};
+
+const fontConfig = {
+  web: fontFamily,
+  ios: fontFamily,
+  android: fontFamily,
+};
 
 const theme = {
   ...DefaultTheme,
+  fonts: configureFonts(fontConfig),
   colors: {
     ...DefaultTheme.colors,
-    primary: peach,
-    accent: peach,
+    primary: '#FCBCC7',
+    accent: '#FCBCC7',
   },
 };
 
