34f0059f3d360b7457701dfffccbfa153dcbbced
SphericalKat <amolele@gmail.com>
2021-05-13 18:28:06 +0530

    refactor(itempage): show user feedback when adding item to cart
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- src/pages/ItemPage.tsx
+++ src/pages/ItemPage.tsx
@@ -2,7 +2,7 @@ import {gql, useMutation, useQuery} from '@apollo/client';
 import {useRoute} from '@react-navigation/core';
 import * as React from 'react';
 import {Text, View, StyleSheet, Image} from 'react-native';
-import {Button} from 'react-native-paper';
+import {Button, Snackbar, useTheme} from 'react-native-paper';
 import Loading from '../components/Loading';
 import {Item} from '../types';
 
@@ -26,6 +26,8 @@ interface CartMutResponse {
 const ItemPage = (_props: ItemPageProps) => {
   const route: any = useRoute();
   const {id, aspectRatio} = route.params!;
+  const [showSnack, setShowSnack] = React.useState('');
+  const theme = useTheme();
 
   const ITEM_QUERY = gql`
     query Item($id: ID!) {
@@ -51,9 +53,23 @@ const ItemPage = (_props: ItemPageProps) => {
       addToCart(data: {inventoryItemId: $id, quantity: $qty})
     }
   `;
-  const [addToCart] = useMutation<CartMutResponse, CartMutVars>(CART_MUTATION, {
+  const [addToCart, {data: mutationData, error: mutationError}] = useMutation<
+    CartMutResponse,
+    CartMutVars
+  >(CART_MUTATION, {
     variables: {id, qty: 1},
   });
+
+  React.useEffect(() => {
+    if (mutationData?.addToCart) {
+      setShowSnack('Added item to cart');
+    }
+
+    if (mutationError) {
+      setShowSnack('Error adding item to cart');
+    }
+  }, [mutationData, mutationError]);
+
   if (loading || error) {
     return <Loading />;
   }
@@ -61,21 +77,33 @@ const ItemPage = (_props: ItemPageProps) => {
   const {InventoryItem: item} = data!;
 
   return (
-    <View style={styles.container}>
-      <Image
-        style={[styles.image, {aspectRatio}]}
-        source={{uri: `${item.imageURL}`}}
-      />
-      <Text style={styles.name}>{item.name}</Text>
-      <Text style={styles.desc}>{item.description}</Text>
-      <Button
-        style={styles.button}
-        mode="contained"
-        onPress={() => {
-          addToCart();
-        }}>
-        Add to cart
-      </Button>
+    <View>
+      <View style={styles.container}>
+        <Image
+          style={[styles.image, {aspectRatio}]}
+          source={{uri: `${item.imageURL}`}}
+        />
+        <Text style={styles.name}>{item.name}</Text>
+        <Text style={styles.desc}>{item.description}</Text>
+        <Button
+          style={styles.button}
+          mode="contained"
+          onPress={() => {
+            addToCart();
+          }}>
+          Add to cart
+        </Button>
+      </View>
+      <Snackbar
+        style={{backgroundColor: theme.colors.primary}}
+        visible={!!showSnack}
+        action={{
+          label: 'Dismiss',
+          onPress: () => setShowSnack(''),
+        }}
+        onDismiss={() => setShowSnack('')}>
+        {showSnack}
+      </Snackbar>
     </View>
   );
 };
