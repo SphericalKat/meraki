24a161eab7fb3b7cf112b9df91aee8d92c09d242
SphericalKat <amolele@gmail.com>
2022-02-19 19:07:05 +0530

    feat: add a route to fetch all products for a category
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- product/views.py
+++ product/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from rest_framework import viewsets, permissions
 
@@ -16,12 +17,31 @@ class LatestProductList(APIView):
 
 
 class ProductViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for product CRUD
+    """
+
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [permissions.IsAdminUser | ReadOnly]
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for categories CRUD
+    """
+
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [permissions.IsAdminUser | ReadOnly]
+
+    @action(detail=True, methods=["get"])
+    def products(self, request, pk=None):
+        """
+        Gets all products belonging to this category
+        """
+        category = Category.objects.get(id=pk)
+        serializer = ProductSerializer(
+            category.products, many=True, context={"request": request}
+        )
+        return Response(serializer.data)
